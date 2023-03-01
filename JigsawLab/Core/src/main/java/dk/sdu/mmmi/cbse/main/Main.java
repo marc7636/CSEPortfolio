fa7c803ffa4ef84d6c33d5d67309ed2bf8f7f062
Marcus Schebye Skrvier <marcus@familienskriver.dk>
2023-03-01 09:03:51 +0100

    Copied JavaLab into JigsawLab

---
+++ JigsawLab/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -0,0 +1,20 @@
+package dk.sdu.mmmi.cbse.main;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Lwjgl3ApplicationConfiguration cfg =
+			new Lwjgl3ApplicationConfiguration();
+		cfg.setTitle("Asteroids");
+		cfg.setWindowedMode(500, 400);
+		cfg.setResizable(false);
+		
+		new Lwjgl3Application(new Game(), cfg);
+		
+	}
+	
+}
