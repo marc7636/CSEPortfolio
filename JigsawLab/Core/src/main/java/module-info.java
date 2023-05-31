229e3de90200070c9283f9655076cb4b50331975
Marcus Skriver <marcus@familienskriver.dk>
2023-05-31 17:14:53 +0200

    Oops...

--- JigsawLab/Core/src/main/java/module-info.java
+++ JigsawLab/Core/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IPreEntityProcessingService;
 
 module Core {
 	requires Common;
@@ -11,6 +12,7 @@ module Core {
 	uses IGamePluginService;
 	uses IEntityProcessingService;
 	uses IPostEntityProcessingService;
+	uses IPreEntityProcessingService;
 	
 	exports dk.sdu.mmmi.cbse.main;
 }
