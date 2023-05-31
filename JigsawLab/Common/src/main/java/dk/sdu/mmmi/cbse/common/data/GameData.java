68539b6bf3561d8e9b21659291d749ea9c97d0c3
Marcus Skriver <marcus@familienskriver.dk>
2023-05-31 20:32:08 +0200

    Removed unused event system

--- JigsawLab/Common/src/main/java/dk/sdu/mmmi/cbse/common/data/GameData.java
+++ JigsawLab/Common/src/main/java/dk/sdu/mmmi/cbse/common/data/GameData.java
@@ -1,30 +1,11 @@
 package dk.sdu.mmmi.cbse.common.data;
 
-import dk.sdu.mmmi.cbse.common.events.Event;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
 public class GameData {
 
     private float delta;
     private int displayWidth;
     private int displayHeight;
     private final GameKeys keys = new GameKeys();
-    private List<Event> events = new CopyOnWriteArrayList<>();
-
-    public void addEvent(Event e) {
-        events.add(e);
-    }
-
-    public void removeEvent(Event e) {
-        events.remove(e);
-    }
-
-    public List<Event> getEvents() {
-        return events;
-    }
 
     public GameKeys getKeys() {
         return keys;
@@ -52,16 +33,5 @@ public class GameData {
 
     public int getDisplayHeight() {
         return displayHeight;
-    }
-
-    public <E extends Event> List<Event> getEvents(Class<E> type, String sourceID) {
-        List<Event> r = new ArrayList<>();
-        for (Event event : events) {
-            if (event.getClass().equals(type) && event.source().getID().equals(sourceID)) {
-                r.add(event);
-            }
-        }
-
-        return r;
     }
 }
--- JigsawLab/Common/src/main/java/module-info.java
+++ JigsawLab/Common/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module Common {
 	exports dk.sdu.mmmi.cbse.common.data;
 	exports dk.sdu.mmmi.cbse.common.data.entityparts;
-	exports dk.sdu.mmmi.cbse.common.events;
 	exports dk.sdu.mmmi.cbse.common.services;
 }
