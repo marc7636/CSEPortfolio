d66499899e26e0a0af218cb75e663af64f238f04
Marcus Skriver <marcus@familienskriver.dk>
2023-05-31 20:32:25 +0200

    Moved Bullet into the bullet module

--- JigsawLab/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
+++ JigsawLab/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
@@ -1,6 +1,5 @@
 package dk.sdu.mmmi.cbse.bulletsystem;
 
-import dk.sdu.mmmi.cbse.common.bullet.Bullet;
 import dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
@@ -45,7 +44,6 @@ public class BulletControlSystem implements IEntityProcessingService, BulletSPI 
         float x = shooterPos.getX();
         float y = shooterPos.getY();
         float radians = shooterPos.getRadians();
-        float dt = gameData.getDelta();
         float speed = 350;
 
         Entity bullet = new Bullet();
--- JigsawLab/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletPlugin.java
+++ JigsawLab/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletPlugin.java
@@ -1,15 +1,11 @@
 package dk.sdu.mmmi.cbse.bulletsystem;
 
-import dk.sdu.mmmi.cbse.common.bullet.Bullet;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 public class BulletPlugin implements IGamePluginService {
-
-    private Entity bullet;
-
     @Override
     public void start(GameData gameData, World world) {
 
